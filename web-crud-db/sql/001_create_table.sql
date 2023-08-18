ea8ef2b77e274dc593970910a7c8a1a90d8c9a82
joaofsoares <joaofsoares@yahoo.com>
2023-08-18 09:17:40 +0100

    pk uuid

--- web-crud-db/sql/001_create_table.sql
+++ web-crud-db/sql/001_create_table.sql
@@ -3,7 +3,8 @@
 CREATE TABLE record(
 	ID VARCHAR(36) NOT NULL,
 	title VARCHAR(100) NOT NULL,
-	description VARCHAR(255)
+	description VARCHAR(255),
+	PRIMARY KEY (ID)
 );
 
 INSERT INTO record (id, title, description) VALUES (uuid(), "Title 1", "Description 1");
